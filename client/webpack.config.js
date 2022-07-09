6742725c5cc14207c22267e9a1c2f7f149a698ff
Sherreka <rekadarnb@gmail.com>
2022-07-09 12:42:16 -0400

    changed the service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -24,6 +25,7 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW(),
       new InjectManifest({
         swSrc: './src/sw.js',
         swDest: 'service-worker.js',
