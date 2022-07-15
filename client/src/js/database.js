c69cfd1e32dd79a30c0a1a71504e6ab94de251ca
Sherreka <rekadarnb@gmail.com>
2022-07-15 11:12:43 -0400

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -30,7 +30,7 @@ export const putDb = async (content) => {
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data saved to the database', result.value);
 
 };
 
@@ -48,12 +48,12 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
    // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result?.value;
 
 };
 
